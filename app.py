11ace864d50fcf71854df0d61c83b683e0ea1215
Morteza <maleki.morteza92@gmail.com>
2023-09-08 12:21:28 +0330

    added the main app

--- app.py
+++ app.py
@@ -1,33 +1,89 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, redirect, url_for, flash
+import json
+from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField
+from wtforms.validators import DataRequired
+from reportlab.pdfgen import canvas
 
 app = Flask(__name__)
-
-# A list to store the notes
+app.secret_key = 'your_secret_key'  # Replace with a secret key for session management
 notes = []
 
+# Note Form
+class NoteForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    
 @app.route('/')
 def index():
-    return render_template('index.html', notes=notes)
+    notes_with_index = [(idx, note) for idx, note in enumerate(notes)]
+    return render_template('index.html', notes=notes_with_index)
 
-@app.route('/add', methods=['POST'])
-def add():
-    note = request.form['note']
-    notes.append(note)
-    return redirect('/')
+@app.route('/create', methods=['GET', 'POST'])
+def create_note():
+    form = NoteForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        content = form.content.data
 
-@app.route('/edit/<int:index>', methods=['GET', 'POST'])
-def edit(index):
-    if request.method == 'POST':
-        note = request.form['note']
-        notes[index] = note
-        return redirect('/')
-    else:
-        return render_template('edit.html', index=index, note=notes[index])
+        current_datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-@app.route('/delete/<int:index>')
-def delete(index):
-    del notes[index]
-    return redirect('/')
+        note = {'title': title, 'content': content, 'created_at': current_datetime}
+        notes.append(note)
+        save_notes_to_json()
+        flash('Note created successfully', 'success')
+        return redirect(url_for('index'))
+    return render_template('create_note.html', form=form)
+
+@app.route('/view/<int:id>')
+def view_note(id):
+    note = notes[id]
+    return render_template('view_note.html', note=note, id=id)
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit_note(id):
+    form = NoteForm()
+    note = notes[id]
+
+    form.title.data = note['title']
+    form.content.data = note['content']
+
+    if form.validate_on_submit():
+        note['title'] = form.title.data
+        note['content'] = form.content.data
+        save_notes_to_json()
+        flash('Note updated successfully', 'success')
+        return redirect(url_for('index'))
+    
+    return render_template('edit_note.html', form=form, note=note, id=id)
+
+
+@app.route('/delete/<int:id>')
+def delete_note(id):
+    del notes[id]
+    save_notes_to_json()
+    flash('Note deleted successfully', 'success')
+    return redirect(url_for('index'))
+
+@app.route('/export/<int:id>')
+def export_to_pdf(id):
+    note = notes[id]
+    response = canvas.Canvas(f'note_{id}.pdf')
+    response.drawString(100, 750, note['title'])
+    response.drawString(100, 700, note['content'])
+    response.showPage()
+    response.save()
+    return redirect(url_for('index'))
+
+def save_notes_to_json():
+    with open('notes.json', 'w') as json_file:
+        json.dump(notes, json_file)
 
 if __name__ == '__main__':
-    app.run()
+    try:
+        with open('notes.json', 'r') as json_file:
+            notes = json.load(json_file)
+    except FileNotFoundError:
+        notes = []
+    app.run(debug=True)
